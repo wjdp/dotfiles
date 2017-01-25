16a245bd3ec6b0f5054787574510ba6ba9d93c66
Will Pimblett <will@wjdp.uk>
2017-01-25 14:34:41 +0000

    Suspend button fix script

---
+++ scripts/fix-power-buttons.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+gsettings set org.gnome.settings-daemon.plugins.power button-suspend "nothing"
+gsettings set org.gnome.settings-daemon.plugins.power button-sleep "nothing"
+gsettings set org.gnome.settings-daemon.plugins.power button-hibernate "nothing"
