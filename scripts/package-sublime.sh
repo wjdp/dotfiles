2c91f444b209ae83707672083b06678f353b5e59
Will Pimblett <will@wjdp.uk>
2017-04-12 18:05:56 +0100

    Add sublime to dotfiles
    +bump version to 3128

--- scripts/package-sublime.sh
+++ scripts/package-sublime.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
 cd /tmp
-wget https://download.sublimetext.com/sublime-text_build-3126_amd64.deb
-sudo dpkg -i sublime-text_build-3126_amd64.deb
+wget https://download.sublimetext.com/sublime-text_build-3128_amd64.deb
+sudo dpkg -i sublime-text_build-3128_amd64.deb
+rm -rf $HOME/.config/sublime-text-3/Packages/User
+ln -s $HOME/dotfiles/sublime $HOME/.config/sublime-text-3/Packages/User
