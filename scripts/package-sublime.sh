cf241da4949168566c51f603b0db8d110d04c46d
Will Pimblett <will@wjdp.uk>
2017-05-02 19:34:28 +0100

    Fix sublime script config for new install

--- scripts/package-sublime.sh
+++ scripts/package-sublime.sh
@@ -3,5 +3,6 @@
 cd /tmp
 wget https://download.sublimetext.com/sublime-text_build-3131_amd64.deb -O subl.deb
 sudo dpkg -i subl.deb
+mkdir -p $HOME/.config/sublime-text-3/Packages/User
 rm -rf $HOME/.config/sublime-text-3/Packages/User
 ln -s $HOME/dotfiles/sublime $HOME/.config/sublime-text-3/Packages/User
