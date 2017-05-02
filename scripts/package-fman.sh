af48c59d9004a982bc28196556de4c52f61a3409
Will Pimblett <will@wjdp.uk>
2017-05-02 19:32:34 +0100

    Add fman and gitkraken install scripts

---
+++ scripts/package-fman.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd /tmp
+wget http://download.fman.io/fman.deb
+sudo dpkg -i fman.deb
---
+++ scripts/package-gitkraken.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd /tmp
+wget https://release.gitkraken.com/linux/gitkraken-amd64.deb
+sudo dpkg -i gitkraken-amd64.deb
