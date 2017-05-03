252d7191bf253b875c88c908028309ab7cf05e92
Will Pimblett <will@wjdp.uk>
2017-05-03 13:15:42 +0100

    Switch puttl to two HDMI cables

--- bin/puttl-dock.sh
+++ bin/puttl-dock.sh
@@ -4,15 +4,14 @@ TOGGLE=$HOME/.docked
 
 if [ ! -e $TOGGLE ]; then
     touch $TOGGLE
-    xrandr --output HDMI-1-1 --auto
-    xrandr --output DP-1-2 --auto
+    xrandr --output HDMI-1-2 --auto
+    xrandr --output HDMI-1-1 --auto --right-of HDMI-1-2
     xrandr --output eDP-1-1 --off
-    xrandr --output HDMI-1-1 --right-of DP-1-2
 else
     rm $TOGGLE
     xrandr --output eDP-1-1 --auto
+    xrandr --output HDMI-1-2 --off
     xrandr --output HDMI-1-1 --off
-    xrandr --output DP-1-2 --off
 fi
 
 
