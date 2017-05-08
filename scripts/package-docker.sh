7aaa80f5daa328251dedd201675a6a780292ccd7
Will Pimblett <will@wjdp.uk>
2017-05-08 11:31:35 +0100

    Also install docker-compose with docker

--- scripts/package-docker.sh
+++ scripts/package-docker.sh
@@ -5,3 +5,9 @@ if [[ "$?" == 1 ]]; then
   curl -sSL https://get.docker.com/ | sudo sh
   sudo usermod -aG docker will
 fi
+
+cd /tmp
+
+wget https://github.com/docker/compose/releases/download/1.13.0/docker-compose-`uname -s`-`uname -m` -O docker-compose
+sudo mv docker-compose /usr/local/bin
+sudo chmod +x /usr/local/bin/docker-compose
