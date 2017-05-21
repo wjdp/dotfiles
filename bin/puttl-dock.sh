2a0b54eda69674e65f6ab785c8ca8e2103276ae9
Will Pimblett <will@wjdp.uk>
2017-05-21 17:36:13 +0100

    puttl things

--- bin/puttl-dock.sh
+++ bin/puttl-dock.sh
@@ -3,17 +3,25 @@
 TOGGLE=$HOME/.docked
 
 if [ ! -e $TOGGLE ]; then
+    # DOCK
     touch $TOGGLE
     xrandr --output HDMI-1-2 --auto
     xrandr --output HDMI-1-1 --auto --right-of HDMI-1-2
     xrandr --output eDP-1-1 --off
+
+    xmodmap -e "keycode 112 = Prior"
+    xmodmap -e "keycode 117 = Next"
+    xmodmap -e "keycode 110 = Home"
+    xmodmap -e "keycode 115 = End"
 else
+    # UNDOCK
     rm $TOGGLE
     xrandr --output eDP-1-1 --auto
     xrandr --output HDMI-1-2 --off
     xrandr --output HDMI-1-1 --off
+
+    xmodmap -e "keycode 112 = Home"
+    xmodmap -e "keycode 117 = End"
+    xmodmap -e "keycode 110 = Prior"
+    xmodmap -e "keycode 115 = Next"
 fi
-
-
-
-
