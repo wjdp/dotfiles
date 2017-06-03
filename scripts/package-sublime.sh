74a5d04a5f8736c28d9ad83d55b9f44fa49d72e3
Will Pimblett <will@wjdp.uk>
2017-06-03 14:57:22 +0100

    Bump sublime version

--- scripts/package-sublime.sh
+++ scripts/package-sublime.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 cd /tmp
-wget https://download.sublimetext.com/sublime-text_build-3131_amd64.deb -O subl.deb
+wget https://download.sublimetext.com/sublime-text_build-3133_amd64.deb -O subl.deb
 sudo dpkg -i subl.deb
 mkdir -p $HOME/.config/sublime-text-3/Packages/User
 rm -rf $HOME/.config/sublime-text-3/Packages/User
 ln -s $HOME/dotfiles/sublime $HOME/.config/sublime-text-3/Packages/User
+
