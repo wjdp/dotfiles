e542cad3785193ab0644c2627fd68d65c43cbf92
Will Pimblett <will@wjdp.uk>
2017-07-29 20:00:30 +0100

    Bump go version to 1.8.3

--- scripts/package-go.sh
+++ scripts/package-go.sh
@@ -2,5 +2,5 @@
 
 cd /tmp
 # This needs bumping to go version desired
-wget https://storage.googleapis.com/golang/go1.7.4.linux-amd64.tar.gz -O go.tar.gz
+wget https://storage.googleapis.com/golang/go1.8.3.linux-amd64.tar.gz -O go.tar.gz
 sudo tar -C /usr/local -xzf go.tar.gz
