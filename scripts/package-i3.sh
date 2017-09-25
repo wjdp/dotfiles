d1f68bffed462ad814ed0b5dfe1cd233f943b558
Will Pimblett <will@wjdp.uk>
2017-09-25 11:20:19 +0100

    Upgrade i3 to their repo version

---
+++ scripts/package-i3.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd /tmp
+/usr/lib/apt/apt-helper download-file http://debian.sur5r.net/i3/pool/main/s/sur5r-keyring/sur5r-keyring_2017.01.02_all.deb keyring.deb SHA256:4c3c6685b1181d83efe3a479c5ae38a2a44e23add55e16a328b8c8560bf05e5f
+sudo dpkg -i ./keyring.deb
+echo "deb http://debian.sur5r.net/i3/ $(grep '^DISTRIB_CODENAME=' /etc/lsb-release | cut -f2 -d=) universe" | sudo tee --append /etc/apt/sources.list.d/sur5r-i3.list
+sudo apt update
+sudo apt install i3 -y
