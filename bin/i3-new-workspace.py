136fc5c84fd0df702c39e9155a802cfb4a7cee32
Will Pimblett <will@wjdp.uk>
2018-02-18 21:30:22 +0000

    Bump

---
+++ bin/i3-new-workspace.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+import i3ipc
+import string
+import random
+
+i3 = i3ipc.Connection()
+
+i3.command("workspace X{}{}".format(random.choice(string.hexdigits), random.choice(string.hexdigits)))
