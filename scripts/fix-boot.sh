2449f8d27f401ba5da3b2943f6942113b699fd7a
Will Pimblett <will@wjdp.uk>
2018-06-14 10:24:19 +0100

    Add boot partition cleaning script

---
+++ scripts/fix-boot.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Ubuntu has a problem, and that problem is the fecking /boot partition filling up
+
+echo $(dpkg -l 'linux-*' | sed '/^ii/!d;/'"$(uname -r | sed "s/\(.*\)-\([^0-9]\+\)/\1/")"'/d;s/^[^ ]* [^ ]* \([^ ]*\).*/\1/;/[0-9]/!d')
+
+dpkg -l 'linux-*' | sed '/^ii/!d;/'"$(uname -r | sed "s/\(.*\)-\([^0-9]\+\)/\1/")"'/d;s/^[^ ]* [^ ]* \([^ ]*\).*/\1/;/[0-9]/!d' | xargs sudo dpkg -r
