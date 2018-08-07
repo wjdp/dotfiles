2e81836d086d51199796ad32fd9af4d23332c7d1
Will Pimblett <will@wjdp.uk>
2018-08-07 10:33:45 +0100

    Add yarn install script

---
+++ scripts/package-yarn.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo echo "--- Installing Yarn"
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt update -qy && sudo apt install yarn -qy
