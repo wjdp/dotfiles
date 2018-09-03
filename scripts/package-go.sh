48c5f9839c5b1be89f6f67b521001c1896ca8d13
Will Pimblett <will@wjdp.uk>
2018-09-03 10:20:22 +0100

    Go 1.11

--- scripts/package-go.sh
+++ scripts/package-go.sh
@@ -2,6 +2,6 @@
 
 cd /tmp
 # This needs bumping to go version desired
-wget https://storage.googleapis.com/golang/go1.10.1.linux-amd64.tar.gz -O go.tar.gz
+wget https://storage.googleapis.com/golang/go1.11.linux-amd64.tar.gz -O go.tar.gz
 sudo rm -rf /usr/local/go
 sudo tar -C /usr/local -xzf go.tar.gz
