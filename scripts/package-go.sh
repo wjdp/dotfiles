3324a5fce351d4055c541b2377331dbdb44f8506
Will Pimblett <will@wjdp.uk>
2019-01-26 02:08:27 +0000

    gopass

--- scripts/package-go.sh
+++ scripts/package-go.sh
@@ -5,3 +5,6 @@ cd /tmp
 wget https://storage.googleapis.com/golang/go1.11.linux-amd64.tar.gz -O go.tar.gz
 sudo rm -rf /usr/local/go
 sudo tar -C /usr/local -xzf go.tar.gz
+
+# Go apps I use
+go get -u github.com/gopasspw/gopass
