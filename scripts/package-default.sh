6702e6ceee4bf248559fd6963759a2f4792b1dc3
Will Pimblett <will@wjdp.uk>
2019-06-03 14:56:16 +0100

    Add autolock

--- scripts/package-default.sh
+++ scripts/package-default.sh
@@ -1,7 +1,7 @@
 # Installs packages I want from the ubuntu index
 
 # WM / System
-sudo apt install -qy i3 lxappearance compton rofi feh supervisor hsetroot
+sudo apt install -qy i3 lxappearance compton rofi feh supervisor hsetroot xautolock
 
 # CLI tools
 sudo apt install -qy git git-lfs zsh vim curl wget httpie ruby ruby-dev htop powertop build-essential python3-pip python3-dev sudo software-properties-common pass xdotool ncdu
