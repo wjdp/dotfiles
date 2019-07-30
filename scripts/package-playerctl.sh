f9bce35d17cf7371b4de67ffef741db56ec21a4c
Will Pimblett <will@wjdp.uk>
2019-07-30 12:10:52 +0100

    Bump version of playerctl

--- scripts/package-playerctl.sh
+++ scripts/package-playerctl.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
+set -e
 cd /tmp
-wget https://github.com/acrisci/playerctl/releases/download/v2.0.1/playerctl-2.0.1_amd64.deb -O playerctl.deb
+wget https://github.com/acrisci/playerctl/releases/download/v2.0.2/playerctl-2.0.2_amd64.deb -O playerctl.deb
 sudo dpkg -i playerctl.deb
 
