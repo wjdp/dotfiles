1a7c92400dfc8a1d8f4788d319df447c8a915936
Will Pimblett <will@wjdp.uk>
2019-08-26 02:12:42 +0100

    Install pass otp

--- scripts/package-pass.sh
+++ scripts/package-pass.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
 go get github.com/gopasspw/gopass
+
+sudo apt install -qy oathtool expect zbar-tools
+
+cd ~/local
+git clone https://github.com/tadfisher/pass-otp
+cd pass-otp
+sudo make install
