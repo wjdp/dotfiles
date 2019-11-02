1372f73135fc9213489b511abf805a2374e1254d
Will Pimblett <will@wjdp.uk>
2019-11-02 16:11:13 +0000

    Upgrade node to 12.x

--- scripts/package-node.sh
+++ scripts/package-node.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 
-curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
 # Upgrade latest
 sudo npm install -g npm
 sudo npm -g update
-
-# My packages
-sudo npm install -g coffeescript bower gulp-cli grunt-cli
