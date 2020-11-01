56a8fd51739f9b4807a88f16c7952bc8c79e4593
Will Pimblett <will@wjdp.uk>
2020-11-01 00:15:35 +0000

    Upgrade docker-compose

--- scripts/package-docker.sh
+++ scripts/package-docker.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+COMPOSE_VERSION=1.27.4
+
 which docker
 if [[ "$?" == 1 ]]; then
   curl -sSL https://get.docker.com/ | sudo sh
@@ -8,6 +10,6 @@ fi
 
 cd /tmp
 
-wget https://github.com/docker/compose/releases/download/1.21.1/docker-compose-`uname -s`-`uname -m` -O docker-compose
+wget https://github.com/docker/compose/releases/download/$COMPOSE_VERSION/docker-compose-`uname -s`-`uname -m` -O docker-compose
 sudo mv docker-compose /usr/local/bin
 sudo chmod +x /usr/local/bin/docker-compose
