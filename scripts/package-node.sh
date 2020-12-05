dd5b8550b33e1e5a43df7fa0f52e0b6732a194c9
Will Pimblett <will@wjdp.uk>
2020-12-05 19:51:37 +0000

    More upgrades / regolith fixes

--- scripts/package-node.sh
+++ scripts/package-node.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
 # Upgrade latest
