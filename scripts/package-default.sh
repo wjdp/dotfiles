443e232b42d989e0be9b0dff5d91e595de719f5b
Will Pimblett <will@wjdp.uk>
2020-12-05 22:01:12 +0000

    Regogo

--- scripts/package-default.sh
+++ scripts/package-default.sh
@@ -3,7 +3,7 @@
 # Installs packages I want from the ubuntu index
 
 # WM / System
-sudo apt install -qy lxappearance rofi feh supervisor hsetroot xautolock pavucontrol pasystray flameshot
+sudo apt install -qy lxappearance rofi feh supervisor hsetroot xautolock pavucontrol pasystray flameshot xautomation
 
 # CLI tools
 sudo apt install -qy git git-lfs zsh vim curl wget httpie ruby ruby-dev htop powertop build-essential python3-pip python3-dev sudo software-properties-common pass xdotool ncdu
