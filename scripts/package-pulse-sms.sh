995de6cf18c644907f4f35969a361c52e40df68b
Will Pimblett <will@wjdp.uk>
2020-12-12 02:53:41 +0000

    Delete package-pulse-sms.sh
    No longer used

--- scripts/package-pulse-sms.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd /tmp
-wget -O pulse-sms.deb https://github.com/klinker-apps/messenger-issues/releases/download/v1.0.0-beta8/pulse-sms-1.0.0-beta8-amd64.deb
-sudo dpkg -i pulse-sms.deb
