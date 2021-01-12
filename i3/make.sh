494631ca648dc37b9008a9e831cfa6b72626e86b
Will Pimblett <will@wegift.io>
2021-01-12 16:00:08 +0000

    Remove old i3

--- i3/make.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "⇒ building i3conf for" `hostname`
-mkdir -p $HOME/.i3
-rm -f $HOME/.i3/config
-cat $HOME/dotfiles/i3/conf/0-common > $HOME/.i3/config
-cat $HOME/dotfiles/i3/conf/`hostname` >> $HOME/.i3/config
-
