e80581f44cf204618024ebdb6bd0a861e9437e93
Will Pimblett <will@wjdp.uk>
2021-03-22 00:01:16 +0000

    fish abbrs

--- fish/conf.d/alias.fish
+++ fish/conf.d/alias.fish
@@ -1,12 +1,7 @@
-alias g=git
-alias gc='git commit'
-
 alias l='ls -l'
 alias ls='lsd'
 alias ll='lsd -lah --date=relative'
 alias lt='ls --tree'
-
-alias dc=docker-compose
 
 alias psync='pass git pull && pass git push'
 
