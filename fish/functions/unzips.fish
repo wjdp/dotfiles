c5f9407e79c1fd358b3cd6f38af3dcff52afb34f
Will Pimblett <will@wjdp.uk>
2021-03-29 17:00:08 +0100

    Add waly and unzips functions

---
+++ fish/functions/unzips.fish
@@ -0,0 +1,6 @@
+# Defined in /tmp/fish.HTFyXV/unzips.fish @ line 1
+function unzips
+  for f in *.zip
+    unzip $f
+  end
+end
---
+++ fish/functions/waly.fish
@@ -0,0 +1,5 @@
+# Defined in /tmp/fish.bEAPHr/waly.fish @ line 1
+function waly
+  wally-cli $argv
+  rm $argv
+end
