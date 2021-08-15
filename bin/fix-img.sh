a13d2be0f504fa5258cc6549be71cb07ab10c449
Will Pimblett <will@wjdp.uk>
2021-08-15 19:08:05 +0100

    Add fix-img.sh script

---
+++ bin/fix-img.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+for f in *.{jpg,JPG,png,PNG,jpeg,JPEG}; do
+    type=$( file "$f" | grep -oP '\w+(?= image data)' )
+    case $type in
+        PNG)  newext=png ;;
+        JPEG) newext=jpg ;;
+        *)    echo "??? what is this: $f"; continue ;;
+    esac
+    ext=${f##*.}   # remove everything up to and including the last dot
+    if [[ $ext != $newext ]]; then
+        # remove "echo" if you're satisfied it's working
+        mv "$f" "${f%.*}.$newext"
+    fi
+done
+
