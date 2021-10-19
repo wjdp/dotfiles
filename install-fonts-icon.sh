5f67f8d41b917ad6b96e82604ca5d8fe73e08f99
Will Pimblett <will@wegift.io>
2021-10-19 15:23:26 +0100

    Remove fonts-icon
    Don't think this was used.

--- install-fonts-icon.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-ln -sf $HOME/dotfiles/fonts-icon/material-design-icons/iconfont/MaterialIcons-Regular.ttf ~/.fonts/
-ln -sf $HOME/dotfiles/fonts-icon/MaterialDesign-Webfont/fonts/materialdesignicons-webfont.ttf ~/.fonts/
-ln -sf "$HOME/dotfiles/fonts-icon/Font-Awesome/otfs/Font Awesome 5 Free-Solid-900.otf" ~/.fonts/
-ln -sf "$HOME/dotfiles/fonts-icon/Font-Awesome/otfs/Font Awesome 5 Free-Regular-400.otf" ~/.fonts/
-ln -sf "$HOME/dotfiles/fonts-icon/Font-Awesome/otfs/Font Awesome 5 Brands-Regular-400.otf" ~/.fonts/
-ln -sf $HOME/dotfiles/fonts-icon/typicons.font/src/font/typicons.ttf ~/.fonts/
-fc-cache -fv
