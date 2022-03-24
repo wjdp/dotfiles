fe0c9b12286c3d856804a1402241b486c1e71ddc
Will Pimblett <will@wegift.io>
2022-03-24 13:38:35 +0000

    Implement multiline prompt

--- roles/fish/files/functions/fish_prompt.fish
+++ roles/fish/files/functions/fish_prompt.fish
@@ -1,25 +1,24 @@
-function fish_prompt
-  set -l last_command_status $status
+function left_prompt
   set -l cwd
 
-  if test "$theme_short_path" = 'yes'
+  if test "$theme_short_path" = yes
     set cwd (basename (prompt_pwd))
   else
     set cwd (prompt_pwd)
   end
 
-  set -l fish     ""
-  set -l error    ""
-  set -l ahead    ""
-  set -l behind   ""
+  set -l fish ""
+  set -l error ""
+  set -l ahead ""
+  set -l behind ""
   set -l diverged "⥄ "
-  set -l dirty    "δ"
-  set -l none     ""
+  set -l dirty δ
+  set -l none ""
 
-  set -l normal_color     (set_color normal)
-  set -l success_color    (set_color 86d0d1)
-  set -l error_color      (set_color red --bold)
-  set -l directory_color  (set_color $fish_color_quote 2> /dev/null; or set_color brown)
+  set -l normal_color (set_color normal)
+  set -l success_color (set_color 86d0d1)
+  set -l error_color (set_color red --bold)
+  set -l directory_color (set_color $fish_color_quote 2> /dev/null; or set_color brown)
   set -l repository_color (set_color $fish_color_cwd 2> /dev/null; or set_color green)
 
   if test $last_command_status -eq 0
@@ -27,9 +26,9 @@ function fish_prompt
   else
     echo -n -s $error_color $error $normal_color
   end
-  
+
   if git_is_repo
-    if test "$theme_short_path" = 'yes'
+    if test "$theme_short_path" = yes
       set root_folder (command git rev-parse --show-toplevel 2> /dev/null)
       set parent_root_folder (dirname $root_folder)
       set cwd (echo $PWD | sed -e "s|$parent_root_folder/||")
@@ -46,6 +45,45 @@ function fish_prompt
   else
     echo -n -s " " $directory_color $cwd $normal_color
   end
+end
 
-  echo -n -s " "
+function right_prompt
+  set -l py_version (python -c 'import sys; print(".".join(map(str, sys.version_info[:3])))')
+  if set -q AWS_VAULT
+    echo -n -s (set_color -b yellow black) "  " (basename "$AWS_VAULT") " " (set_color normal) " "
+  end
+  if set -q VIRTUAL_ENV
+    echo -n -s (set_color -b blue white) "  " (basename "$VIRTUAL_ENV")@$py_version" " (set_color normal) " "
+  end
+  if test $last_command_status != 0
+    echo -n -s (set_color -b red white) " $last_command_status " (set_color normal) " "
+  end
+  set_color 474747
+  date "+%H:%M:%S"
+  set_color normal
+end
+
+function padding
+  set -l space ""
+  for i in (seq 1 $argv[1])
+    set space " "$space
+  end
+  set_color -b black
+  printf $space
+  set_color normal
+end
+
+function remove_color
+  printf $argv | perl -pe 's/\x1b.*?[mGKH]//g'
+end
+
+function fish_prompt
+  set -g last_command_status $status
+  set -l left (left_prompt)
+  set -l right (right_prompt)
+  # set -g paddding (padding (math $COLUMNS - (remove_color "$left$right" | string length)))
+  # set -l cols (math $COLUMNS - (remove_color "$left$right" | string length))
+  printf "\n"
+  printf "$left $right"
+  printf "\n "
 end
--- roles/fish/files/functions/fish_right_prompt.fish
+++ roles/fish/files/functions/fish_right_prompt.fish
@@ -1,19 +1,3 @@
 function fish_right_prompt
-  set -l last_command_status $status
 
-  if set -q AWS_VAULT
-    echo -n -s " "
-    echo -n -s (set_color -b yellow black) "  " (basename "$AWS_VAULT") " " (set_color normal) " "
-  end
-  if set -q VIRTUAL_ENV
-    echo -n -s " "
-    echo -n -s (set_color -b blue white) "  " (basename "$VIRTUAL_ENV") " " (set_color normal) " "
-  end
-  if test $last_command_status != 0
-    echo -n -s " "
-    echo -n -s (set_color -b red white) " $last_command_status " (set_color normal) " "
-  end
-  set_color 474747
-  date "+%H:%M:%S"
-  set_color normal
 end
