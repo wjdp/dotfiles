3f7fe680f677176e2220ac2ad4d8857efe1d2265
Will Pimblett <will@wjdp.uk>
2023-02-15 23:16:47 +0000

    Remove unused scripts

--- scripts/package-1password.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sudo apt-key --keyring /usr/share/keyrings/1password.gpg adv --keyserver keyserver.ubuntu.com --recv-keys 3FEF9748469ADBE15DA7CA80AC2D62742012EA22
-echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/1password.gpg] https://downloads.1password.com/linux/debian edge main' | sudo tee /etc/apt/sources.list.d/1password.list
-sudo apt-get -qqy update
-sudo apt-get install -qqy 1password
