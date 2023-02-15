fa19b2f320b47060c19dd272dd168a413aecaf43
Will Pimblett <will@wjdp.uk>
2023-02-15 22:54:21 +0000

    Move fish abbreviations to config file
    Using abbr command doesn't seem to save them anymore. Removed in 3.6.0.

---
+++ roles/fish/files/conf.d/abbr.fish
@@ -0,0 +1,9 @@
+# Abbreviations
+
+abbr -ag dc "docker compose"
+abbr -ag g "git"
+abbr -ag gc "git commit"
+abbr -ag p "gopass"
+abbr -ag tf "terraform"
+abbr -ag avx "aws-vault exec"
+abbr -ag avl "aws-vault login"
