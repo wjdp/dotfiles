e7bb96cfc1ff15fa81ba21c0e7ca33c3e4dec514
Will Pimblett <will@wjdp.uk>
2024-08-01 14:32:19 +0100

    Alias for pnpm

--- roles/fish/files/conf.d/abbr.fish
+++ roles/fish/files/conf.d/abbr.fish
@@ -3,7 +3,7 @@
 abbr -ag dc "docker compose"
 abbr -ag g "git"
 abbr -ag gc "git commit"
-abbr -ag p "gopass"
+abbr -ag p "pnpm"
 abbr -ag tf "terraform"
 abbr -ag avx "aws-vault exec"
 abbr -ag avl "aws-vault login"
