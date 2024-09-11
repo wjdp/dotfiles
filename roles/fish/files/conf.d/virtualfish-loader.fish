31edf4f6b87fa2d089c778de3745964c6001577d
Will Pimblett <will@wegift.io>
2024-09-11 10:26:05 +0100

    Update virtualfish python

--- roles/fish/files/conf.d/virtualfish-loader.fish
+++ roles/fish/files/conf.d/virtualfish-loader.fish
@@ -1,4 +1,4 @@
 set -g VIRTUALFISH_VERSION 2.5.9
-set -g VIRTUALFISH_PYTHON_EXEC /home/will/.local/pipx/venvs/virtualfish/bin/python
-source /home/will/.local/pipx/venvs/virtualfish/lib/python3.10/site-packages/virtualfish/virtual.fish
+set -g VIRTUALFISH_PYTHON_EXEC /home/will/.local/share/pipx/venvs/virtualfish/bin/python
+source /home/will/.local/share/pipx/venvs/virtualfish/lib/python3.12/site-packages/virtualfish/virtual.fish
 emit virtualfish_did_setup_plugins
