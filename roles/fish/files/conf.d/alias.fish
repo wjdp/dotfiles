45a77ee3926c4f925a9c731be49c3f05c27631d9
Will Pimblett <will@wjdp.uk>
2025-08-07 15:49:06 +0100

    Add fish alias for claude

--- roles/fish/files/conf.d/alias.fish
+++ roles/fish/files/conf.d/alias.fish
@@ -4,6 +4,7 @@ alias ll='lsd -lah --date=relative'
 alias lt='ls --tree'
 
 alias psync='pass git pull && pass git push'
+alias claude='pnpx @anthropic-ai/claude-code'
 
 function prj
   cd ~/local/$argv
