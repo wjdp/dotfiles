38bc829acbb515f6f951227b63573994dcbcfd04
Will Pimblett <will@wjdp.uk>
2025-10-21 22:04:23 +0100

    Update pyenv.fish

--- roles/fish/files/conf.d/pyenv.fish
+++ roles/fish/files/conf.d/pyenv.fish
@@ -1,3 +1,13 @@
-set -gx PYENV_ROOT $HOME/.pyenv
-status is-login; and pyenv init --path | source
-fish_add_path $PYENV_ROOT/bin
+# Add pyenv executable to PATH by running
+# the following interactively:
+
+set -Ux PYENV_ROOT $HOME/.pyenv
+set -U fish_user_paths $PYENV_ROOT/bin $fish_user_paths
+
+# Load pyenv automatically by appending
+# the following to ~/.config/fish/config.fish:
+
+pyenv init - fish | source
+
+
+# Restart your shell for the changes to take effect.
