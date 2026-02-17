04f5da6f69d3e483121b701d45b11c6a255fb77f
Will Pimblett <will@wjdp.uk>
2026-02-17 20:49:09 +0000

    Remove claude pnpm alias

--- roles/fish/files/conf.d/alias.fish
+++ roles/fish/files/conf.d/alias.fish
@@ -4,7 +4,6 @@ alias ll='lsd -lah --date=relative'
 alias lt='ls --tree'
 
 alias psync='pass git pull && pass git push'
-alias claude='pnpx @anthropic-ai/claude-code'
 
 function prj
   cd ~/local/$argv
